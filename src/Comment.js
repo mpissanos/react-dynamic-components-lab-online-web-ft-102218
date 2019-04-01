6daf6a7e982c55643ae7ff26080f7e7cf24aeaf9
Manny Pissanos <mpissano@gmail.com>
2019-04-01 18:45:11 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,9 @@
-class Comment
+import React, { Component } from 'react';
+
+class Comment extends Component {
+	render() {
+		return <div className="comment">{this.props.commentText}</div>;
+	}
+}
+
+export default Comment;
